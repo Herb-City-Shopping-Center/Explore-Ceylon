7452534e5d504ba24d48b0e8946b1293ee41e06c
RandikaVS <83448005+RandikaVS@users.noreply.github.com>
2023-04-13 16:12:01 +0530

    Admin dashboard create

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import {Route} from 'react-router-dom'
-import HomePage from './pages/HomePage';
 import LoginPage from './pages/Authentication/LoginPage';
 import SignupPage from './pages//Authentication/SignupPage';
 import ProfilePage from './pages/ProfilePage';
 import Navigation from './components/Navigation';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={HomePage} exact />
-      <Route path="/signup" component={SignupPage} exact />
-      <Route path="/login" component={LoginPage} exact />
-      <Route path="/profile" component={ProfilePage} exact />
+      <Route path="/admin" component={Dashboard} exact />
+      <Route path="/admin/signup" component={SignupPage} exact />
+      <Route path="/admin/login" component={LoginPage} exact />
+      <Route path="/admin/profile" component={ProfilePage} exact />
     </div>
   );
 }
